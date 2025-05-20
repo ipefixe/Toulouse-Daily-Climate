dd784f21e40ca08ebfc4bcef4531c9a91ce4a64d
Ipefixe <legauloisfr@gmail.com>
2025-05-20 11:56:32 +0200

    Add a logger

---
+++ Toulouse Weather/Extensions/Logger+.swift
@@ -0,0 +1,16 @@
+//
+//  Logger+.swift
+//  Toulouse Weather
+//
+//  Created by Kevin Boulala on 20/05/2025.
+//
+
+import OSLog
+
+extension Logger {
+    private static var identifier = Bundle.main.bundleIdentifier ?? "fr.ipefixe.toulouse-weather"
+
+    static let persistent = Logger(subsystem: identifier, category: "PersistentService")
+    static let scraping = Logger(subsystem: identifier, category: "ScrapingService")
+    static let app = Logger(subsystem: identifier, category: "App")
+}
